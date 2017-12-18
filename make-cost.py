d31845c666d6f89988e4ad7d7da3d56024641bb6
bjohnson <bkj.322@gmail.com>
2017-12-17 23:53:46 -0500

    cleanup

--- make-cost.py
+++ make-cost.py
@@ -12,9 +12,9 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dim', type=int, default=1024 << 4)
+    parser.add_argument('--dim', type=int, default=1024 << 5)
     parser.add_argument('--max', type=int, default=256)
-    parser.add_argument('--outpath', type=str, default="cost.txt")
+    parser.add_argument('--outpath', type=str, default="cost")
     
     args = parser.parse_args()
     assert args.max == 256
@@ -24,6 +24,13 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
+    
+    print("generating %dx%d matrix, max entry %d" % (args.dim, args.dim, args.max), file=sys.stderr)
     cost = np.random.choice(args.max, (args.dim, args.dim)).astype(int)
-    np.savetxt(args.outpath, cost, fmt='%d', delimiter=' ')
+    
+    print("saving %s.npy" % args.outpath, file=sys.stderr)
+    np.save(args.outpath, cost)
+    
+    print("saving %s.txt" % args.outpath, file=sys.stderr)
+    np.savetxt(args.outpath + '.txt', cost, fmt='%d', delimiter=' ')
 
